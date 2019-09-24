f17b05760972bfcde3d02204c96a107c31e7f3a0
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-09-24 15:01:03 -0400

    install bcryptjs and set up initial user model

---
+++ api/routes/users/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+
+  if (user.isModified('password') || user.isNew) {
+    try {
+      const hash = await bcrypt.hash(user.password, 10);
+      user.password = hash;
+      return next();
+    } catch (e) {
+      return next(e);
+    }
+  } else {
+    return next();
+  }
+});
+
+exports.model = mongoose.model('User', userSchema);
