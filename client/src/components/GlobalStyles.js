df82ab8804bea9e9311cb776087cbf4eb8ca56b6
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-10-02 13:19:55 -0400

    initial global styles

---
+++ client/src/components/GlobalStyles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+	@import url('https://fonts.googleapis.com/css?family=Montserrat|Nunito:700&display=swap');
+	* {
+		font-family: 'Montserrat', sans-serif;
+	}
+`;
