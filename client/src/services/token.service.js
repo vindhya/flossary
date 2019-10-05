d18d39bd53869d818d18635c0d8fb03e93e9bfdb
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-10-05 18:55:45 -0400

    created token service

---
+++ client/src/services/token.service.js
@@ -0,0 +1,11 @@
+export const setToken = token => {
+  localStorage.setItem('token', token);
+};
+
+export const getToken = () => {
+  return localStorage.getItem('token');
+};
+
+export const removeToken = () => {
+  localStorage.removeItem('token');
+};
