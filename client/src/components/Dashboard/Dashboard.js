5141052422a66c7fad8a125f2eda65331b104efc
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-10-05 23:49:43 -0400

    initial rendering of floss lists

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,8 +2,14 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { UserContext } from '../user-context';
 import { getToken } from '../../services/token.service';
+import FlossList from '../Floss/FlossList';
 
 const getUsername = email => email.slice(0, email.indexOf('@'));
+
+const renderFlossLists = listArray => {
+  console.log('floss lists inside renderflosslists', listArray);
+  return listArray.map(list => <FlossList data={list} key={list._id} />);
+};
 
 const Dashboard = () => {
   const [flossLists, setFlossLists] = useState([]);
@@ -30,6 +36,8 @@ const Dashboard = () => {
   return (
     <div className="mt-3">
       <h4>Welcome, {getUsername(userData.email)}!</h4>
+      <p>These are your floss lists:</p>
+      {renderFlossLists(flossLists)}
     </div>
   );
 };
---
+++ client/src/components/Floss/FlossList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FlossList = () => {
+  return <div>This is a floss list!</div>;
+};
+
+export default FlossList;
