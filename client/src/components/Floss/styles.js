fa364464acd62e03838cc37150f1932c3619d6b9
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-10-06 01:26:18 -0400

    added border radius and changed margin

--- client/src/components/Floss/styles.js
+++ client/src/components/Floss/styles.js
@@ -4,6 +4,7 @@ export const ColouredSquare = styled.div`
   width: 1rem;
   height: 1rem;
   background: ${props => `rgb(${props.red},${props.green},${props.blue})`};
+  border-radius: 3px;
   display: inline-block;
-  margin-right: 0.5rem;
+  margin-right: 0.7rem;
 `;
